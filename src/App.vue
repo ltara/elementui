d8f8823d492231bfb7f16dfc931b348281f9034c
caidengfeng <caidengfeng@qianxin.com>
2022-01-19 14:46:37 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello App!</h1>
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/input">el-input</router-link>
+    </p>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  h1 {
+    text-align: center;
+  }
+
+  .container {
+    width: 800px;
+    height: 400px;
+    border: 1px solid #dddddd;
+    padding: 16px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+import Input from './components/input/index.vue'
+import InputBasic from './components/input/basic.vue'
+import AutoComplete from './components/input/auto-complete.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
+const routes = [
+  {
+    path: '/input',
+    component: Input,
+    children: [
+      { path: '/input/basic', component: InputBasic },
+      { path: '/input/auto-complete', component: AutoComplete },
+    ],
+  },
+]
+
+const router = new VueRouter({
+  routes, // (缩写) 相当于 routes: routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
